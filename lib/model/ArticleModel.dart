76076f735c0def6d570f86d62d81909b38f7159c
mcleaw <48169447+mcleaw@users.noreply.github.com>
2021-05-28 20:31:22 +0800

    update

--- lib/model/ArticleModel.dart
+++ lib/model/ArticleModel.dart
@@ -1,10 +1,10 @@
-class ArticleModel{
-  String title;
-  String description;
-  String author;
-  String urlToImage;
-  String url;
-  String content;
+  class ArticleModel{
+    String title;
+    String description;
+    String author;
+    String urlToImage;
+    String url;
+    String content;
 
-  ArticleModel({this.author,this.title,this.url,this.description,this.content,this.urlToImage});
-}
+    ArticleModel({this.author,this.title,this.url,this.description,this.content,this.urlToImage});
+  }
